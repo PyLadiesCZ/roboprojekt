9171f5211c0785839790f33aada0f47422418bd6
Katerina Jureckova <katka.jureckova@gmail.com>
2018-11-06 23:06:40 +0100

    Map changed to test_3.json - map with layers and squares rotation

--- game.py
+++ game.py
@@ -7,7 +7,7 @@ TILE_HEIGHT = 64
 WINDOW_WIDTH = 12*TILE_WIDTH
 WINDOW_HEIGHT = 12*TILE_HEIGHT
 
-map_name = "./maps/test_2.json"
+map_name = "./maps/test_3.json"
 data = backend.get_data(map_name)
 
 window = frontend.init_window(WINDOW_WIDTH, WINDOW_HEIGHT)
@@ -18,14 +18,11 @@ state = backend.get_coordinate_dict(coordinates, tilelist)
 
 images = frontend.load_images(data, state, TILE_WIDTH, TILE_HEIGHT)
 
-#print('coordinates', coordinates)
-#print('tilelist', tilelist)
-#print('state', state)
-#print('images', images)
 
 @window.event
 def on_draw():
     window.clear()
     frontend.draw_board(state, images)
+
 
 pyglet.app.run()
