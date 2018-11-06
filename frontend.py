211813b5e436d62e916b3cbe7446af38ed561981
Katerina Jureckova <katka.jureckova@gmail.com>
2018-11-06 23:05:56 +0100

    Adding rotation for squares

--- frontend.py
+++ frontend.py
@@ -10,18 +10,19 @@ def init_window(WINDOW_WIDTH, WINDOW_HEIGHT):
 '''this function make a list of images'''
 def load_images(data, state, TILE_WIDTH, TILE_HEIGHT):
     real_images = backend.get_real_ids(data)
-    print(state)
     images = []
     for layer in state:
-        for key in state[layer]:
-            if state[layer][key] in real_images:
-                img = pyglet.image.load(real_images[state[layer][key]])
+        for key, value in state[layer].items():
+            coordinate, rotation = value
+            if coordinate in real_images:
+                img = pyglet.image.load(real_images[coordinate])
                 img.anchor_x = img.width//2
                 img.anchor_y = img.height//2
                 x, y = key
                 tile_x = x*TILE_WIDTH
                 tile_y = y*TILE_HEIGHT
                 img = pyglet.sprite.Sprite(img, x=img.anchor_x+tile_x, y=img.anchor_y+tile_y)
+                img.rotation = rotation
                 images.append(img)
     return images
 
