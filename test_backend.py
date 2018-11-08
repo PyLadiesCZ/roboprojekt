a3b512e53ef55fa4ecdd2a3b42e6971a80e633b4
Karolina <33810531+befeleme@users.noreply.github.com>
2018-11-08 09:18:57 +0100

    Update test_backend.py

--- test_backend.py
+++ test_backend.py
@@ -1,9 +1,6 @@
 from backend import get_tiles, get_coordinates, get_data
 import pytest
 
-
-
-
 @pytest.mark.parametrize('map_name', ['test_1', 'test_2', 'test_3'])
 def test_get_coordinates_returns_list(map_name):
     """Test that get_coordinates() returns a list for each map."""
