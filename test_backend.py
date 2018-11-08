937989c6ca11447582be24bdf9fbd6ca16e50a7c
Karolina <33810531+befeleme@users.noreply.github.com>
2018-11-08 09:18:35 +0100

    Update according to Terka's commit
    Terka removed the 1st test because of failing - code has changed.

--- test_backend.py
+++ test_backend.py
@@ -1,14 +1,8 @@
 from backend import get_tiles, get_coordinates, get_data
 import pytest
 
-@pytest.mark.parametrize('x', [3, 'asd', (1, 'bl', 7)])  
-def test_get_tiles(x):
-    """Test that get_tiles() returns value under key 'data' of the 0th element of the
-    array under key 'layers', no matter what it actually is.
-    This is just a simple test to try it out, not particularly useful.
-    """
-    data = {'layers' : [{'data' : x}]}
-    assert get_tiles(data) == x
+
+
 
 @pytest.mark.parametrize('map_name', ['test_1', 'test_2', 'test_3'])
 def test_get_coordinates_returns_list(map_name):
