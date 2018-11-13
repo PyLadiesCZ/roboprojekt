6bfaf11faf782a02f87c09b6ea1346df440e3571
Helena K <amazonkaarya@seznam.cz>
2018-11-13 12:50:05 +0100

    Update frontend.py

--- frontend.py
+++ frontend.py
@@ -1,17 +1,47 @@
-# this file is still in progress
+# this file is still in progress #WIP
+
+'''
+The module frontend is part of RoboProject by Pyladies Brno.
+
+Key library is pyglet Python library  https://bitbucket.org/pyglet/pyglet/wiki/Home
+This module imports the backend module and is imported in game module.
+
+The frontend module
+    - creates a Pyglet window for drawing
+    - loads pyglet sprites
+    - draws images to display the game board
+'''
+
 import pyglet
 import backend
 
-'''this function make  window for drawing'''
+
 def init_window(WINDOW_WIDTH, WINDOW_HEIGHT):
+    '''
+    creates a pyglet window for graphic output
+
+    is called in the game module and uses arguments
+    WINDOW_WIDTH and WINDOW_HEIGHT from the game module
+    '''
     window = pyglet.window.Window(WINDOW_WIDTH, WINDOW_HEIGHT)
     return window
 
-'''this function make a list of images'''
+
 def load_images(data, state, TILE_WIDTH, TILE_HEIGHT):
+    '''
+    makes a list of images
+
+    calls backends' get_real_ids function (returning a dictionary of IDs)
+    creates empty list of images and fills it with data from JSON
+    (including layers, coordinates, rotation)
+    '''
     real_images = backend.get_real_ids(data)
     images = []
+
+    #filling the empty list of images
     for layer in state:
+        # state is distioary created in backends function get_coordiante_dict
+# !!!!!!!!!!!!!!!!!!! tuhle cast asi jeste rozsirit !!!!!!!!!!!!!!!!
         for key, value in state[layer].items():
             coordinate = value.id
             rotation = value.rotation
@@ -27,9 +57,11 @@ def load_images(data, state, TILE_WIDTH, TILE_HEIGHT):
                 images.append(img)
     return images
 
-'''this function is drawing a map'''
-def draw_board(state, images):
 
+def draw_board(state, images):
+    '''
+    draws the game map
+    '''
     for tile in images:
         tile.draw()
 
