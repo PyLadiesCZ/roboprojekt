da478882d98f97f55ce38859155ab809299d00a2
befeleme <w_oczekiwaniu@interia.pl>
2018-12-04 18:48:42 +0100

    Rename class Direction attributes, extend docstring.

--- backend.py
+++ backend.py
@@ -38,7 +38,7 @@ class Robot:
         """
 
         (x, y) = self.coordinates
-        (new_x, new_y) = direction.vector
+        (new_x, new_y) = direction.coor_delta
         x = x + (new_x * distance)
         y = y + (new_y * distance)
 
@@ -67,7 +67,7 @@ class Direction(Enum):
     S = 180, (0, -1), 2
     W = 270, (-1, 0), 3
 
-    def __new__(cls, keycode, vector, tile_property):
+    def __new__(cls, degrees, coor_delta, tile_property):
         """
         Get attributes value and vector of the given Direction class values.
 
@@ -76,11 +76,14 @@ class Direction(Enum):
         tile_property: map tile property: value (custom - added in Tiled).
         Makes it possible to change vector and tile_property when the object is rotated.
         With degrees change (value) there comes the coordinates (vector) change and tile_property.
+
+        More info about enum - official documentation: https://docs.python.org/3/library/enum.html
+        Blog post with the exact __new__() usage: http://xion.io/post/code/python-enums-are-ok.html 
         """
         obj = object.__new__(cls)
-        obj._value_ = keycode
-        obj.vector = vector
-        obj.tile_property = tile_property
+        obj._value_ = degrees
+        obj.coor_delta = coor_delta
+        obj.map_property = tile_property
         return obj
 
     def get_new_direction(self, where_to):
