7885d5b78aa29807e33fdbfe9d877c8a48597aa4
vahalova <vahalova@gmail.com>
2018-12-10 10:40:00 +0100

    Add a bad test_map, add an invalid test

--- test_backend.py
+++ test_backend.py
@@ -195,3 +195,7 @@ def test_tile_size(map_name):
 @pytest.mark.parametrize("map_name", ["test_3", "test_5"])
 def test_map_is_valid(map_name):
     assert img_list(map_name) == True
+
+@pytest.mark.parametrize("map_name", ["test_6"])
+def test_map_is_invalid(map_name):
+    assert img_list(map_name) != True
