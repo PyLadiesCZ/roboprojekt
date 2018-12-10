250efbbea52b466d0c611360e463bb5f3634a612
vahalova <vahalova@gmail.com>
2018-12-10 15:57:28 +0100

    M

--- validator.py
+++ validator.py
@@ -3,16 +3,16 @@ from backend import get_board, get_data
 
 def get_order_squares(text):
     order_squares = {
-    'ground':"A",
-    'hole':"B",
-    'starting_square':"B",
-    'repair':"B",
-    'belt':"B",
-    'gear':"B",
-    'flag':"C",
-    'pusher':"D",
-    'laser':"D",
-    'wall':"D"}
+    'ground':"A_ground",
+    'hole':"B_hole",
+    'starting_square':"B_starting_square",
+    'repair':"B_repair",
+    'belt':"B_belt",
+    'gear':"B_gear",
+    'flag':"C_flag",
+    'pusher':"D_pusher",
+    'laser':"D_laser",
+    'wall':"D_wall"}
     return order_squares[text]
 
 def check_squares(map_name):
@@ -24,17 +24,28 @@ def check_squares(map_name):
     """
     for coordinate, type in board.items():
         square_type_letter = []
+        square_type_and_direction = []
         for i in type:
             square_type_letter.append(get_order_squares(i.type))
+
+            """
+            Squares with the same type and direction mustn't be in list of types
+            """
+            if (i.type, i.direction) in square_type_and_direction:
+                return coordinate, i.type # (8, 9), 'wall'
+            else:
+                square_type_and_direction.append((i.type, i.direction))
+
         a = 0
         b = 0
         c = 0
+
         for letter in square_type_letter:
-            if letter == 'A':
+            if letter[0] == 'A':
                 a += 1
-            if letter == 'B':
+            if letter[0] == 'B':
                 b += 1
-            if letter == 'C':
+            if letter[0] == 'C':
                 c += 1
         if a > 1 or b > 1 or c > 1:
             return coordinate, a, b, c # ((1, 9), 1, 2, 0)
@@ -44,9 +55,22 @@ def check_squares(map_name):
         ["A","B","D"] is correct ["D", "A"] is not
         A < B < C < D
         """
-        letter_count = len(square_type_letter)
-        for i in range(letter_count-1):
-            if square_type_letter[i] > square_type_letter[i+1]:
+        letters_count = len(square_type_letter)
+        for i in range(letters_count-1):
+            if square_type_letter[i][0] > square_type_letter[i+1][0]:
                 return coordinate, square_type_letter[i] # ((7, 9), 'D')
-                
+
+        """
+
+        """
+        for i in square_type_and_direction:
+            type, direction = i
+
+
+            if type == 'hole'  or type == 'starting_square':
+                return coordinate, type # (8, 10), 'hole')
+
+
+
+
     return True
