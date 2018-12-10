2835bbedabca1524baddd6531d2264bdfb25b87f
Iveta Cesalova <cesalova.iveta@seznam.cz>
2018-12-10 15:42:14 +0100

    export svg to png

---
+++ export_img.py
@@ -0,0 +1,14 @@
+import sys
+import subprocess
+from pathlib import Path
+
+base = Path(".")
+inkscape = "C:/Program Files/Inkscape/inkscape"
+def prevod_svg_png():
+    for soubor in base.glob("*.svg"):
+        jmeno = str(soubor)
+        nove_jmeno = jmeno[:-4] + ".png"
+        subprocess.run([inkscape, jmeno, "--export-png=" + nove_jmeno, "--export-area-page"],check = True,)
+
+prevod_svg_png()
+print("Done")
