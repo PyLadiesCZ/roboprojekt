6177a345ed78581702a714abdf4a47cf0f3590b2
ivet1987 <39279854+ivet1987@users.noreply.github.com>
2018-12-18 10:53:55 +0100

    uložení png do jiné složky (#135)
    * uložení png do jiné šložky
    
    * uložení png do jiné složky
    
    * uložení png do jiné složky
    
    * úprava odsazení

--- export_img.py
+++ export_img.py
@@ -2,7 +2,7 @@
 Program export images in svg format to png format.
 Open file in directory where you have svg images saved.
 """
-
+import os
 import sys
 import subprocess
 from pathlib import Path
@@ -38,21 +38,33 @@ def find_inkscape_path():
 
 inkscape = find_inkscape_path()
 
+def create_png_directory():
+    """
+    Check if png directory exist, if not create new one
+    """
+    dir = "../png"
+    if os.path.isdir(dir):
+        pass
+    else:
+        os.mkdir(dir)
+
 def export_svg_png():
     """
-    Export img in svg to img.png
+    Export img.svg to img.png with Inkscape
     """
     for img in base.glob("*.svg"):
         name = str(img)
         parts = []
         new_name = ""
+
         for part in img.with_suffix(".png").parts:
             if part == "svg":
                 parts.append("png")
             else:
                 parts.append(part)
             new_name = str(Path(*parts))
-        subprocess.run([inkscape, name, "--export-png="+ new_name, "--export-area-page"],check = True,)
+        subprocess.run([inkscape, name,"--export-png=" + "../png/" +new_name,"--export-area-page"],check = True,)
 
+create_png_directory()
 export_svg_png()
 print("Done")
