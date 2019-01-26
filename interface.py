bbb23dd2ed474128586b5ec1e294de20e635e951
vahalova <vahalova@gmail.com>
2019-01-26 15:24:44 +0100

    Add docstring and resize window

--- interface.py
+++ interface.py
@@ -12,12 +12,19 @@ class InterfaceState:
         self.robot_data = robot_data
         self.my_cards = [None, None, None, None, None]
         self.power_down = False
-        self.cursor_index = 0 # 0-4
+        self.cursor_index = 0 # 0-4 number of positon
 
     def __repr__(self):
         return "<InterfaceState Cards: {}, My Cards: {}, Power Down: {}, Robot: {}>".format(self.deal_cards, self.my_cards, self.power_down, self.robot_data)
 
+
+
     def select_card(self, deal_card_index):
+        """
+        Select a card from the table and put on
+        a place where selector is (in your "hand")
+        and selector cursor moves to the next free place.
+        """
         if deal_card_index >= len(self.deal_cards):
             return
         if self.deal_cards[deal_card_index] not in self.my_cards:
@@ -27,26 +34,43 @@ class InterfaceState:
 
     # Return one card back on the teble
     def return_card(self):
+        """
+        Return one selected card from your hand back to the table.
+        """
         self.my_cards[self.cursor_index] = None
 
 
     # Return all cards back on the table
     def return_cards(self):
+        """
+        Retrun all cards of your hand back to the table.
+        """
+
         self.my_cards = [None, None, None, None, None]
         self.cursor_index = 0
 
 
     def cursor_index_plus(self):
+        """
+        Change selecting cursor position to the next one.
+        """
         if self.cursor_index < 4:
             self.cursor_index += 1
 
 
     def cursor_index_minus(self):
+        """
+        Change selecting cursor position to the previous one.
+        """
         if self.cursor_index > 0:
             self.cursor_index -= 1
 
 
     def switch_power_down(self):
+        """
+        Switch power down status between True and False.
+        When it is True the Robot doesn't play this round.
+        """
         if self.power_down == False:
             self.power_down = True
         else:
@@ -61,7 +85,7 @@ def create_card_pack():
                 'right': [18, 200, 299],
                 'move1': [18, 300, 399],
                 'move2': [12, 400, 499],
-                'move3': [6, 500, 599]
+                'move3': [6, 500, 599],
                 }
     card_pack = []
     for name, number in cards_types.items():
