c5be2f8eef0d95ff1b0bb48c43fdde72e6d78f60
Katerina Jureckova <katka.jureckova@gmail.com>
2019-02-02 17:11:09 +0100

    Update backend tests.

--- test_backend.py
+++ test_backend.py
@@ -1,5 +1,5 @@
 from backend import get_starting_coordinates, get_robot_paths, get_robots_to_start, get_start_state, Robot, State, MovementCard, RotationCard, apply_tile_effects
-from util import Tile, HoleTile, WallTile, GearTile, PusherTile, LaserTile, StartTile, RepairTile, Direction, Rotation
+from util import Tile, RepairTile, Direction, Rotation
 from loading import get_board
 from pathlib import Path
 import pytest
@@ -17,7 +17,8 @@ def test_starting_coordinates():
 
 def test_robot_paths():
     """
-    Get list of robot paths, assert that instance of the list is Path object. The list will change in time, it is not possible to test length or all the paths.
+    Get list of robot paths, assert that instance of the list is Path object.
+    The list will change in time, it is not possible to test length or all the paths.
     """
     robot_paths = get_robot_paths()
     path, path_front = robot_paths[0]
@@ -27,7 +28,8 @@ def test_robot_paths():
 
 def test_robots_on_starting_coordinates():
     """
-    Assert that the result of get_robots_to_start is a list which contains Robot objects with correct attribute coordinates.
+    Assert that the result of get_robots_to_start is a list which contains
+    Robot objects with correct attribute coordinates.
     """
     board = get_board("maps/test_3.json")
     robots = get_robots_to_start(board)
@@ -37,7 +39,8 @@ def test_robots_on_starting_coordinates():
 
 def test_starting_state():
     """
-    Assert that created starting state (board and robots) contains the correct instances of objects.
+    Assert that created starting state (board and robots) contains
+    the correct instances of objects.
     """
     ss = get_start_state("maps/test_3.json")
     assert isinstance(ss, State)
@@ -85,7 +88,7 @@ def test_robot_move(input_coordinates, input_direction, distance, output_coordin
 
 
 @pytest.mark.parametrize(("current_direction", "towards", "new_direction"),
-                        [(Direction.N, Rotation.LEFT, Direction.W),
+                         [(Direction.N, Rotation.LEFT, Direction.W),
                          (Direction.S, Rotation.RIGHT, Direction.W),
                          (Direction.E, Rotation.U_TURN, Direction.W)])
 def test_robot_change_direction(current_direction, towards, new_direction):
@@ -113,27 +116,24 @@ def test_robot_is_repaired(damages_before, tile, damages_after, game_round):
     assert robot.damages == damages_after
 
 
-@pytest.mark.parametrize(("tile", "coordinates_after", "game_round"),
-                         [(RepairTile(None, None, [{'value': True}]), (0, 0), 1),
-                         (RepairTile(None, None, [{'value': False}]), (1, 1), 2),
-                         (RepairTile(None, None, [{'value': True}]), (0, 0), 3),
-                         (RepairTile(None, None, [{'value': False}]), (1, 1), 4),
+@pytest.mark.parametrize(("tile", "coordinates_after"),
+                         [(RepairTile(None, None, [{'value': True}]), (0, 0)),
+                         (RepairTile(None, None, [{'value': False}]), (1, 1)),
                           ])
-def test_robot_changed_start_coordinates(tile, coordinates_after, game_round):
+def test_robot_changed_start_coordinates(tile, coordinates_after):
     robot = Robot(None, None, None, (0, 0))
     state = State({(0, 0): [tile]}, [robot], 1)
-    state.game_round = game_round
     robot.start_coordinates = (1, 1)
     apply_tile_effects(state)
     assert robot.start_coordinates == coordinates_after
 
 
 @pytest.mark.parametrize(("card", "new_coordinates"),
-                        [(MovementCard(100, 1), (5, 6)),
+                         [(MovementCard(100, 1), (5, 6)),
                          (MovementCard(100, 2), (5, 7)),
                          (MovementCard(100, 3), (5, 8)),
                          (MovementCard(100, -1), (5, 4)),
-                         ])
+                          ])
 def test_move_cards(card, new_coordinates):
     """
     Give mock robot the MovementCard and check if he moved to the expected coordinates.
@@ -146,10 +146,10 @@ def test_move_cards(card, new_coordinates):
 
 
 @pytest.mark.parametrize(("card", "new_direction"),
-                        [(RotationCard(100, Rotation.LEFT), Direction.W),
+                         [(RotationCard(100, Rotation.LEFT), Direction.W),
                          (RotationCard(100, Rotation.RIGHT), Direction.E),
                          (RotationCard(100, Rotation.U_TURN), Direction.S),
-                         ])
+                          ])
 def test_rotate_cards(card, new_direction):
     """
     Give mock robot the RotationCard and check if he's heading to the expected direction.
