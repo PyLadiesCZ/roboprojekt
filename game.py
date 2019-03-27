31281d900540160a56e7a7d84b487aa4b120da35
ivet1987 <39279854+ivet1987@users.noreply.github.com>
2019-03-27 10:19:23 +0100

    Create game.py

---
+++ game.py
@@ -0,0 +1,59 @@
+"""
+The game module
+    - coordinate everything and run the game
+    - call pyglet window, various backend and frontend functions
+    - choose standard or other map to be loaded
+"""
+import pyglet
+import sys
+
+from backend import get_start_state, apply_tile_effects
+from frontend import create_window, draw_state
+
+
+# load JSON map data from the backend module
+if len(sys.argv) == 1:
+    map_name = "maps/test_3.json"
+
+# if other map should be loaded, use extra argument "maps/MAP_NAME.json"
+# when calling game.py by Python
+# for example: python game.py maps/test_2.json
+else:
+    map_name = sys.argv[1]
+
+# Get start state of the game from the backend module.
+state = get_start_state(map_name)
+
+# Load pyglet graphic window from the frontend module.
+window = create_window(state)
+
+
+@window.event
+def on_draw():
+    """
+    Draw the game state (board and robots).
+    """
+
+    window.clear()
+    draw_state(state, window)
+
+
+def move_once(t):
+    """
+    Move all robots according to mock cards on hand and perform tile effects.
+    """
+
+    for robot in state.get_active_robots():
+        # robot.apply_card_effect(state)
+        robot.walk(3, state)
+        # print(robot)
+
+    apply_tile_effects(state)
+    print("After tile effects:")
+    for robot in state.robots:
+        print(robot)
+
+pyglet.clock.schedule_once(move_once, 3)
+
+# Run the pyglet library.
+pyglet.app.run()
