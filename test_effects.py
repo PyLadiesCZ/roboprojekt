ed17335260ad75cfdb6fa6bdde935c29c5d5fc34
befeleme <w_oczekiwaniu@interia.pl>
2019-05-05 11:49:20 +0200

    Add decoding actions into robot's program (for moves), start preparing decoding rotations.

--- test_effects.py
+++ test_effects.py
@@ -2,26 +2,68 @@ import pytest
 import yaml
 
 from backend import get_start_state, get_start_tiles, apply_tile_effects
+from backend import MovementCard, RotationCard
+from util import Rotation
+
+
+CARD_TYPES = {
+    "move": MovementCard,
+    "rotate": RotationCard,
+}
+
+ROTATIONS = {
+    "left": Rotation.LEFT,
+    "right": Rotation.RIGHT,
+    "u_turn": Rotation.U_TURN,
+}
 
 
 def get_commands():
+    """
+    Open the file with yaml commands for robots.
+    Currently opens only one file.
+    """
     adr = "tests/test_gear/commands.yaml"
     with open(adr) as f:
         data = yaml.safe_load(f)
     return data
 
 
-def get_actions(data):
-    actions = data['actions']
+def get_test_cards(data):
+    """
+    Decode the first part of yaml command - actions.
+    Transform actions to robot's program
+    (list of cards assigned to 1st, 2nd, etc. robots).
+    """
 
-    return actions
+    yaml_actions = data['actions']
 
+    # Create dictionary: for every robot empty list to be filled with cards
+    robots_program = {robot_number: [] for robot_number in yaml_actions.keys()}
 
-def decode_actions():
-    pass
+    for robot_number, actions in yaml_actions.items():
+        robot_cards = robots_program[robot_number]
+
+        for action in actions:
+
+            # Create the correct Card instance with read properties.
+            # Add the card to robot_actions list and assign the list to the key.
+            robot_card = CARD_TYPES[action["type"]](
+                action["priority"],
+                action["value"],
+                )
+            robot_cards.append(robot_card)
+            robots_program[robot_number] = robot_cards
+
+    return robots_program
 
 
 def decode_results():
+    """
+    Decode the results that are not visible on the map, eg. robot's damages,
+    lives, flags.
+    Use for comparing the expected state of game with the given one.
+    """
     pass
 
 
