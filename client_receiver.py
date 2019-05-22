d4b12b3bb865db603c55f68d9edef2cccd278388
Iveta Česalová <cesalova.iveta@seznam.cz>
2019-05-22 13:57:41 +0200

    connection asyncio + pyglet adjust robot_from_dict, state_from_dict

--- client_receiver.py
+++ client_receiver.py
@@ -6,11 +6,27 @@ import asyncio
 import sys
 import aiohttp
 import json
+import pyglet
 
-from backend import state_from_dict
+from backend import State
+from frontend import draw_state
+
+window = pyglet.window.Window()
+
+state = None
+
+@window.event
+def on_draw():
+    """
+    Draw the game state (board and robots).
+    """
+    window.clear()
+    if state:
+        draw_state(state, window)
 
 
 async def client():
+    global state
     async with aiohttp.ClientSession() as session:
         async with session.ws_connect('http://localhost:8080/ws/') as ws:
             # Waiting for message from server and print them
@@ -19,7 +35,23 @@ async def client():
                 if msg.type == aiohttp.WSMsgType.TEXT:
                     message = msg.data
                     state_dict = json.loads(message)
-                    state = state_from_dict(state_dict)
+                    state = State.from_dict(state_dict)
                     print(state_dict)
 
-asyncio.run(client())
+
+def tick_asyncio(dt):
+    """
+    Schedule an event loop.
+    More about event loop - https://docs.python.org/3/library/asyncio-eventloop.html
+    """
+    loop = asyncio.get_event_loop()
+    # Run the "asyncio.sleep" coroutine until complete
+    loop.run_until_complete(asyncio.sleep(0))
+
+pyglet.clock.schedule_interval(tick_asyncio, 1/30)
+
+# Schedule the "client" task
+# More about Futures - official documentation https://docs.python.org/3/library/asyncio-future.html
+asyncio.ensure_future(client())
+# Run the pyglet library.
+pyglet.app.run()
